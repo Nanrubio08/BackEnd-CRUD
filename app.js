32026fd99c75e75f5183713215a6dc7a96381cae
Hernan Rubio <nahnrub@gmail.com>
2025-08-11 21:14:36 -0500

    Status del proyecto antes viaje

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const app = express();
 const port = 3000;
 const routerProducto = require('./routes/routeProducto');
 const routerCliente = require('./routes/routeCliente');
+const cors = require('cors');
 
 conectarDB();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
+
 app.use('/api/producto', routerProducto);
 app.use('/api/cliente', routerCliente);
-
-
 
 app.listen(port, () => {
     console.log("Servidor activo en el puerto "+ port)
